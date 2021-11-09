6115319267f5aa0569c86da262ba5cde250c3a67
amir <abedi@sohagroup.org>
2021-11-09 10:06:18 +0330

    first test

---
+++ src/main/java/org/sohagorup/education/personrabbit/PersonRabbitApplication.java
@@ -0,0 +1,13 @@
+package org.sohagorup.education.personrabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonRabbitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonRabbitApplication.class, args);
+    }
+
+}
